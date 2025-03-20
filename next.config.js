e515cb27863bbf9ad9e982678b728521830e0f5c
Scott Benson <skobyn@gmail.com>
2025-03-20 14:55:27 -0500

    Fix build configuration: Remove next export command and headers

--- next.config.js
+++ next.config.js
@@ -31,19 +31,6 @@ const nextConfig = {
       "cdn.sanity.io",
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
